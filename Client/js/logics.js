4682e15b2a34f2230b08ecaa8a4705f3091c7332
yalisofer94 <yalisofer@gmail.com>
2020-12-27 16:46:27 +0200

    Started testing google maps api + add game page

--- Client/js/logics.js
+++ Client/js/logics.js
@@ -39,34 +39,6 @@ function recreateRestTable(restaurants) {
     } 
 }
 
-function recreateUsersTable(restaurants) {
-    $("table").empty().remove();
-    $("#users-list").empty();
-    $("#users-list").append(
-        '<h2>Users list</h2>'
-    );
-    rests.map(item => {
-
-        $("#users-list").append(
-
-            '<br>' +
-            '<div class="new-user" style="color:' + item.color + ';' + 'margin-left:30px">' +
-            '<img src="' + item.avatar + '" style="display:block;position:absolute; margin-top:30px">' +
-            '<div class="user-details" style="margin-left:80px;">' +
-            '<p>' +
-            'First Name: ' + item.first_name + '<br>' +
-            'Last Name: ' + item.last_name + '<br>' +
-            'Geneder: ' + item.gender + '<br>' +
-            'Email: ' + item.email + '<br>' +
-            'Color: ' + item.color + '<br>' +
-            'Job: ' + item.job + '<br>' +
-            '<p> </div>' +
-            '</div>'
-        );
-    })
-
-}
-
 function operationsListeners() {
     $("#show-button").click(() => {
         getAllRestaurants();
