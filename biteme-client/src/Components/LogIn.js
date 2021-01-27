3beb97f66f29c9effdaac08fa117f0a6d464bed6
yalisofer94 <yalisofer@gmail.com>
2021-01-27 12:34:45 +0200

    finishing login page - structure

--- biteme-client/src/Components/LogIn.js
+++ biteme-client/src/Components/LogIn.js
@@ -2,6 +2,7 @@ import React ,{ Component} from 'react';
 import Footer from './Footer';
 import Logo from './Logo';
 import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
 import "./App.css";
 
 class Login extends Component {
@@ -10,20 +11,23 @@ class Login extends Component {
             <div>
                 <Logo />
                     <div className="home-content">
-                        <Grid container alignItems="center" justify="center" spacing={0} direction="column">
-                            <h2>Login Your Account</h2>
-                            <form>
-                                <div>
-                                    <label htmlFor="userLoginEmail">Email</label>
-                                    <input type="email" className="forms" id="userLoginEmail" placeholder="Email" onChange={(e) => this.setState({userLoginEmail: e.target.value})} />
+                        <Grid container alignItems="center" justify="center" spacing={0} direction="column" style={{height:'100%'}}>
+                            <h1 style={{marginBottom:'5%'}}>Login Your Account</h1>
+                            <div style={{width:'100%',display: 'block', textAlign: 'center'}}>
+                            <form style={{ display: 'inline-block', marginLeft: 'auto',marginRight: 'auto',textAlign: 'left'}}>
+                                <div style={{marginBottom:'5%', width:'300px'}}>
+                                    {/* <label style={{paddingRight:'14%', fontSize:'20px', fontWeight:'bold'}}>Email</label>
+                                    <input type="email" className="forms" id="userLoginEmail" placeholder="Email" style={{borderLeft:'1px solid'}}onChange={(e) => this.setState({userLoginEmail: e.target.value})} /> */}
+                                    <TextField id="outlined-basic" label="Email" id="userLoginEmail" variant="outlined" />
                                 </div>
-                                <div>
-                                    <label htmlFor="userLoginPassword">Password</label>
-                                    <input type="password" className="forms" id="userLoginPassword" placeholder="Password" onChange={(e) => this.setState({userLoginPassword: e.target.value})} />
+                                <div style={{marginBottom:'5%'}}>
+                                    {/* <label style={{fontSize:'20px', fontWeight:'bold'}}>Password</label>
+                                    <input type="password" className="forms" id="userLoginPassword" placeholder="Password" style={{borderLeft:'1px solid'}} onChange={(e) => this.setState({userLoginPassword: e.target.value})} /> */}
+                                    <TextField id="outlined-password-input" label="Password" id="userLoginPassword" type="password" autoComplete="current-password" variant="outlined"/>
                                 </div>
-                                <button type="submit" className="btn" onClick={this.handleLoginNowBtn}><b>Login Now</b></button>
+                                <button type="submit" className="btn" style={{marginBottom:'5%'}} onClick={this.handleLoginNowBtn}><b>Login Now</b></button>
                             </form>
-                            <p>Don't have an account yet? <span className="cursor-pointer text-warning" onClick={this.handleForms}>Create an Account</span></p>
+                            </div>
                         </Grid>
                     </div>
                 <Footer />
